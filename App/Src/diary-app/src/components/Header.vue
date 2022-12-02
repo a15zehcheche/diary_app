67a2c68889ed231adc1b832fc1dd322b284aca10
ZehaoChen <a15zehcheche@iam.cat>
2022-12-02 13:17:16 +0100

    make header

---
+++ App/Src/diary-app/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="the-header">
+        <img src="@/assets/logo.png" width="40"  alt="" srcset="">
+        <div class="title">Title</div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .the-header{
+        height: 60px;
+        background-color: var(--c-3);
+        color: var(--c-title);
+        display: flex;
+        align-items:center;
+    }
+    .the-header > .title{
+        color: var(--c-title);
+    }
+
+
+</style>
